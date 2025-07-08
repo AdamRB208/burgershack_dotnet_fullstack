d287c320d37911ee4cdd75f6c61a23faac4b28f6
AdamRB208 <adamrborup@gmail.com>
2025-07-08 12:30:44 -0600

    Created Burgers Service

--- server/Controllers/BurgersController.cs
+++ server/Controllers/BurgersController.cs
@@ -4,12 +4,12 @@ namespace burgershack_dotnet_fullstack.Controllers;
 [Route("api/[controller]")]
 public class BurgersController : ControllerBase
 {
-  // public BurgersController(BurgersService burgersService)
-  // {
-  //   _burgersService = burgersService;
-  // }
+  public BurgersController(BurgersService burgersService)
+  {
+    _burgersService = burgersService;
+  }
 
-  // private readonly BurgersService _burgersService;
+  private readonly BurgersService _burgersService;
 
   [HttpGet("test")]
   public ActionResult<string> Test()
--- server/Services/BurgersService.cs
+++ server/Services/BurgersService.cs
@@ -2,5 +2,13 @@ namespace burgershack_dotnet_fullstack.Services;
 
 public class BurgersService
 {
-  
+
+  public BurgersService(BurgersRepository burgersRepository)
+  {
+    _burgersRepository = burgersRepository;
+  }
+  private readonly BurgersRepository _burgersRepository;
+
 }
+
+
