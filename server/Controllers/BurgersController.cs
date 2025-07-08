bf6aa398359c71a9726b6ad3e71b7513452ebaba
AdamRB208 <adamrborup@gmail.com>
2025-07-08 12:08:38 -0600

    Created Burgers Controller

---
+++ server/Controllers/BurgersController.cs
@@ -0,0 +1,16 @@
+namespace burgershack_dotnet_fullstack.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BurgersController : ControllerBase
+{
+  public BurgersController(BurgersService burgersService)
+  {
+    _burgersService = burgersService;
+  }
+
+  private readonly BurgersService _burgersService;
+
+  
+
+}
---
+++ server/Services/BurgersService.cs
@@ -0,0 +1,6 @@
+namespace burgershack_dotnet_fullstack.Services;
+
+public class BurgersService
+{
+  
+}
