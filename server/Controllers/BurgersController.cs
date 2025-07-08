c131e084701de0cb24f6f80a46da3a43d364cfcb
AdamRB208 <adamrborup@gmail.com>
2025-07-08 12:22:45 -0600

    burgers controller is running.

--- server/Controllers/BurgersController.cs
+++ server/Controllers/BurgersController.cs
@@ -4,13 +4,17 @@ namespace burgershack_dotnet_fullstack.Controllers;
 [Route("api/[controller]")]
 public class BurgersController : ControllerBase
 {
-  public BurgersController(BurgersService burgersService)
+  // public BurgersController(BurgersService burgersService)
+  // {
+  //   _burgersService = burgersService;
+  // }
+
+  // private readonly BurgersService _burgersService;
+
+  [HttpGet("test")]
+  public ActionResult<string> Test()
   {
-    _burgersService = burgersService;
+    return Ok("Burgers Controller is running!");
   }
-
-  private readonly BurgersService _burgersService;
-
-  
 
 }
