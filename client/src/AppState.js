9233494949355cd362807f2e110bb73e7b9a597e
AdamRB208 <adamrborup@gmail.com>
2025-07-10 22:53:35 -0600

    created side model, added to appstate and created side card

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -7,6 +7,8 @@ export const AppState = reactive({
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
   /** @type {import('./models/Burger.js').Burger []} user info from the database*/
-  burgers: []
+  burgers: [],
+  /** @type {import('./models/Side.js').Side []} user info from the database*/
+  sides: []
 })
 
---
+++ client/src/models/Side.js
@@ -0,0 +1,9 @@
+
+export class Side {
+  constructor(data){
+    this.id = data.id
+    this.name = data.name
+    this.price = data.price
+    this.imgUrl = data.imgUrl
+  }
+}
